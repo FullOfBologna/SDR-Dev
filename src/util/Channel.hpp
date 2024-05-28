f7cdb3d9be19cea1a0747d2b77600aff67a6271a
Jeff Myers <j.todd.atkins@gmail.com>
2024-05-28 06:27:20 -0600

    Finishing up Channel implmentation. YET TO BE BUILT

--- src/util/Channel.hpp
+++ src/util/Channel.hpp
@@ -11,25 +11,43 @@ public:
     ~Channel();
 
     //Attempt to allow Channel to push whenever. It's possible this will lock indefinitely if either it's unable to acquire the lock, or 
-    void put(std::array<T,MAXIMUM_BUFFER_SIZE>& dataBuffer, size_t bufferSize){
+    void put(std::array<T,MAXIMUM_BUFFER_SIZE>& dataBuffer, size_t bufferSize)
+    {
         std::unique_lock<std::mutex> lock(m_mutex);
-//      m_cond.wait(lock, [&](){return data_ready});
+        m_cond.wait(lock, [&](){return data_ready});
         //type of auto = std::array<T,MAXIMUM_BUFFER_SIZE>::iterator 
-        for(auto it = dataBuffer.begin(); it != dataBuffer.end(); ++it)
+        m_currentBufferSize = bufferSize;
+
+        if(m_currentBufferSize < MAXIMUM_BUFFER_SIZE)
         {
-            if(it > (dataBuffer.begin() + bufferSize))
-            {
-                break;
-            }
-            auto data = *it;
-            m_buffer.push(data);
+            m_currentBufferSize = bufferSize;
+        }else{
+            m_currentBufferSize = (size_t)MAXIMUM_BUFFER_SIZE;
         }
-        m_cond.notify_one();
+
+        std::copy(dataBuffer.begin(), dataBuffer.begin() + m_currentBufferSize, m_buffer.begin());
+        
+        m_condVar.notify_one();
+    }
+
+
+    void std::array<T,MAXIMUM_BUFFER_SIZE>& receive(std::array<T, MAXIMUM_BUFFER_SIZE>& outputBuffer)
+    {
+        
+        // Needs to be a way to handle communicating the buffer size between the two classes. 
+        std::unique_lock<std::mutex> lock(m_mutex);
+        // Setup the wait handler
+        m_condVar.wait(lock, [&](){return !data_ready});
+
+        // Rely on put() to handle setting the current          
+        std::copy(m_buffer.begin(), m_buffer.begin()+m_currentBufferSize, outputBuffer.begin());
+
     }
 
 private:
     std::condition_variable m_condVar;
     std::mutex m_mutex;
-    std::queue<T> m_buffer; //Decision Point between using a queue and using an array
+    std::array<T,MAXIMUM_BUFFER_SIZE> m_buffer; //Decision Point between using a queue and using an array
+    size_t m_currentBufferSize;                                                
                             
 }
